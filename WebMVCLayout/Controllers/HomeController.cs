d1792a396994ad320d393e15aaa0dcb24eef2e9e
Deborah Delgado <debypop7@hotmail.com>
2021-11-09 21:05:10 -0300

    Agregar archivos de proyecto.

---
+++ WebMVCLayout/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMVCLayout.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Fecha = DateTime.Now.ToShortDateString();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Saludo = "Ejemplo en About";
+            return View();
+
+        }
+    }
+}
